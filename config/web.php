1b49612ad13c31279dce94be80e5e0ec9551bf82
admin <kukla.os@gmail.com>
2015-02-25 17:46:08 +0200

    Change git ignore file

---
+++ config/web.php
@@ -0,0 +1,110 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '1jc8PLaruO9YNbSZLY42phFKlYygaX9L',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        /*'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],*/
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => array(
+                'login/<service:google|facebook|etc>' => 'site/login',
+            ),
+            //'suffix' => '.html',
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+        'eauth' => array(
+            'class' => 'nodge\eauth\EAuth',
+            'popup' => true, // Use the popup window instead of redirecting.
+            'cache' => false, // Cache component name or false to disable cache. Defaults to 'cache' on production environments.
+            'cacheExpire' => 0, // Cache lifetime. Defaults to 0 - means unlimited.
+            'httpClient' => array(
+                // uncomment this to use streams in safe_mode
+                //'useStreamsFallback' => true,
+            ),
+            'services' => array( // You can change the providers and their classes.
+                'google' => array(
+                    'class' => 'nodge\eauth\services\GoogleOpenIDService',
+                    //'realm' => '*.example.org', // your domain, can be with wildcard to authenticate on subdomains.
+                ),
+            ),
+        ),
+        'authClientCollection' => [
+            'class' => 'yii\authclient\Collection',
+            'clients' => [
+                'google' => [
+                    'class' => 'yii\authclient\clients\GoogleOpenId'
+                ],
+                /*'facebook' => [
+                    'class' => 'yii\authclient\clients\Facebook',
+                    'clientId' => 'facebook_client_id',
+                    'clientSecret' => 'facebook_client_secret',
+                ],*/
+            ],
+        ],
+        'i18n' => array(
+            'translations' => array(
+                'eauth' => array(
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@eauth/messages',
+                ),
+            ),
+        ),
+    ],
+    'modules' => [
+        'admin' => [
+            'class' => 'app\modules\admin\site',
+        ],
+        'user' => [
+            'class' => 'dektrium\user\Module',
+            'enableUnconfirmedLogin' => true,
+            'admins' => ['testname'],
+        ],
+        'gii' => 'yii\gii\Module',
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
